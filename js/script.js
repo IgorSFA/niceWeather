db9901015aee4d5a8d9815d3c33e2b1eb5027ed6
Igor <igorsfdea@gmail.com>
2015-07-02 00:18:07 -0300

    start styling

--- js/script.js
+++ js/script.js
@@ -22,15 +22,15 @@ function fail(error){
 }
 
 function getWeather(lat, lon){
-	$.get('http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&lang=pt',
-	function(data){
-		console.log(data);
-		//$("#content").text(data.weather[0].main);
-		date = new Date(data.dt);
-		var $weather = '<p>'+ data.weather[0].main +'</p>',
-			$temp = '<p>'+ data.main.temp + 'º</p>',
-			$city = '<p>' + data.name + '</p>',
-			$date = '<p>' + date + '</p>';
+	$.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric',
+	function(response){
+		console.log(response);
+		//$("#content").text(response.weather[0].main);
+		date = new Date();
+		var $weather = '<p>'+ response.weather[0].main +'</p>',
+			$temp = '<p>'+ response.main.temp + 'º</p>',
+			$city = '<p>' + response.name + '</p>',
+			$date = '<p>' + date.getDate()+'/'+ (date.getMonth()+1) + '</p>';
 		$('#content').html($weather + $temp + $city + $date);
 	});
 }
